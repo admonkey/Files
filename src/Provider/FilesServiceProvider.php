50e79aeec6f1defc6da1decac1af8cd3549f1ea9
Egemen Berker Kızılcan <egemenkizilcan@gmail.com>
2016-07-24 20:11:19 +0300

    Updated travis.yml

--- src/Provider/FilesServiceProvider.php
+++ src/Provider/FilesServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider;
 
 class FilesServiceProvider extends ServiceProvider {
 
-    const version = '2.0.0';
+    const version = '2.0.1';
 
     /**
      * Indicates if loading of the provider is deferred.
